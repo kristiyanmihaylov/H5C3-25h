c78efd13f0c5e3ac2ce17d063d49f21a6c56e170
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-08-06 12:34:03 +0200

    added JS for button switch color

--- 6. Netflix LP/js/main.js
+++ 6. Netflix LP/js/main.js
@@ -1,1 +1,17 @@
-const
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+// this is how to grab from the DOM/site items 
+
+// Select tab content item
+function selectItem(e){
+    // Add border to current tab & remove from the others
+    removeBorder();
+    this.classList.add('tab-border');
+}
+
+function removeBorder(){
+    tabItems.forEach(item=>item.classList.remove('tab-border'));
+}
+
+// Listen for tab click
+tabItems.forEach(item=>item.addEventListener('click', selectItem));
