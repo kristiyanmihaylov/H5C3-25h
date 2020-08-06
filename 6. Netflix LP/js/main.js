e2a36d023e3a96879d6037c6d4d67585b4af350f
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-08-06 13:00:24 +0200

    finished 6th but table page cannot get it to work properly

--- 6. Netflix LP/js/main.js
+++ 6. Netflix LP/js/main.js
@@ -6,11 +6,21 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 function selectItem(e){
     // Add border to current tab & remove from the others
     removeBorder();
+    removeShow();
     this.classList.add('tab-border');
+    // Grab content item from DOM
+    console.log(this.id);
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
 }
 
 function removeBorder(){
     tabItems.forEach(item=>item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+    tabContentItems.forEach(item=>item.classList.remove('show'));
 }
 
 // Listen for tab click
